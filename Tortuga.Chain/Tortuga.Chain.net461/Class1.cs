79b6774f73f41b340f0ffedf77fd1a466197186d
Jonathan Allen <jonathanallen@kpmg.com>
2016-02-16 01:50:34 -0800

    Project stubs

---
+++ Tortuga.Chain/Tortuga.Chain.net461/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tortuga.Chain.net461
+{
+    public class Class1
+    {
+    }
+}
