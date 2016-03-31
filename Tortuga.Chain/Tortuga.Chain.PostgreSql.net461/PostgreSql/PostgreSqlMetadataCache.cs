94bab569b4a375a6d4e568d3c9066a78f28874a5
docevaad <tomcik.david@gmail.com>
2016-03-31 00:00:23 -0700

    Continued work on metadata class.

--- Tortuga.Chain/Tortuga.Chain.PostgreSql.net461/PostgreSql/PostgreSqlMetadataCache.cs
+++ Tortuga.Chain/Tortuga.Chain.PostgreSql.net461/PostgreSql/PostgreSqlMetadataCache.cs
@@ -1,4 +1,5 @@
 ﻿using Npgsql;
+using NpgsqlTypes;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
@@ -7,42 +8,101 @@ using Tortuga.Chain.Metadata;
 
 namespace Tortuga.Chain.PostgreSql
 {
-    public class PostgreSqlMetadataCache : DatabaseMetadataCache<PostgreSqlObjectName, DbType>
+    public class PostgreSqlMetadataCache : DatabaseMetadataCache<PostgreSqlObjectName, NpgsqlDbType>
     {
         private readonly NpgsqlConnectionStringBuilder m_ConnectionBuilder;
-        private readonly ConcurrentDictionary<PostgreSqlObjectName, TableOrViewMetadata<PostgreSqlObjectName, DbType>> m_Tables = new ConcurrentDictionary<PostgreSqlObjectName, TableOrViewMetadata<PostgreSqlObjectName, DbType>>();
+        private readonly ConcurrentDictionary<PostgreSqlObjectName, TableOrViewMetadata<PostgreSqlObjectName, NpgsqlDbType>> m_Tables = new ConcurrentDictionary<PostgreSqlObjectName, TableOrViewMetadata<PostgreSqlObjectName, NpgsqlDbType>>();
 
         public PostgreSqlMetadataCache(NpgsqlConnectionStringBuilder connectionBuilder)
         {
             m_ConnectionBuilder = connectionBuilder;
         }
 
-        public override StoredProcedureMetadata<PostgreSqlObjectName, DbType> GetStoredProcedure(PostgreSqlObjectName procedureName)
+        public override StoredProcedureMetadata<PostgreSqlObjectName, NpgsqlDbType> GetStoredProcedure(PostgreSqlObjectName procedureName)
         {
             throw new NotImplementedException();
         }
 
-        public override TableFunctionMetadata<PostgreSqlObjectName, DbType> GetTableFunction(PostgreSqlObjectName tableFunctionName)
+        public override TableFunctionMetadata<PostgreSqlObjectName, NpgsqlDbType> GetTableFunction(PostgreSqlObjectName tableFunctionName)
         {
             throw new NotImplementedException();
         }
 
-        public override TableOrViewMetadata<PostgreSqlObjectName, DbType> GetTableOrView(PostgreSqlObjectName tableName)
+        public override TableOrViewMetadata<PostgreSqlObjectName, NpgsqlDbType> GetTableOrView(PostgreSqlObjectName tableName)
         {
             return m_Tables.GetOrAdd(tableName, GetTableOrViewInternal);
         }
 
-        private TableOrViewMetadata<PostgreSqlObjectName, DbType> GetTableOrViewInternal(PostgreSqlObjectName tableName)
+        private TableOrViewMetadata<PostgreSqlObjectName, NpgsqlDbType> GetTableOrViewInternal(PostgreSqlObjectName tableName)
         {
-            const string tableSql =
-                @"SELECT ";
+            const string TableSql =
+                @"SELECT
+                table_schema as schemaname,
+                table_name as tablename,
+                table_type as type
+                FROM information_schema.tables
+                WHERE table_schema=@Schema AND
+                      table_name=@Name AND
+                      (table_type='BASE TABLE' OR
+                       table_type='VIEW');";
 
-            return null;
+            string actualSchema;
+            string actualName;
+            bool isTable;
+
+            using (var con = new NpgsqlConnection(m_ConnectionBuilder.ConnectionString))
+            {
+                con.Open();
+                using (var cmd = new NpgsqlCommand(TableSql, con))
+                {
+                    cmd.Parameters.AddWithValue("@Schema", tableName.Schema);
+                    cmd.Parameters.AddWithValue("@Name", tableName.Name);
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        if (!reader.Read())
+                            throw new MissingObjectException($"Could not find table or view {tableName}");
+                        actualSchema = reader.GetString(reader.GetOrdinal("schemaname"));
+                        actualName = reader.GetString(reader.GetOrdinal("tablename"));
+                        var type = reader.GetString(reader.GetOrdinal("type"));
+                        isTable = type.Equals("BASE TABLE");
+                    }
+                }
+            }
+
+            var columns = GetColumns(tableName);
+            return new TableOrViewMetadata<PostgreSqlObjectName, NpgsqlDbType>(new PostgreSqlObjectName(actualSchema, actualName), isTable, columns);
         }
 
-        private List<ColumnMetadata<DbType>> GetColumns(PostgreSqlObjectName tableName)
+        private List<ColumnMetadata<NpgsqlDbType>> GetColumns(PostgreSqlObjectName tableName)
         {
-            return null;
+            const string ColumnSql =
+                @"SELECT 
+                column_name,
+                data_type
+                FROM information_schema.columns
+                WHERE table_schema=@Schema AND
+                      table_name=@Name";
+
+            var columns = new List<ColumnMetadata<NpgsqlDbType>>();
+            using (var con = new NpgsqlConnection(m_ConnectionBuilder.ConnectionString))
+            {
+                con.Open();
+                using (var cmd = new NpgsqlCommand(ColumnSql, con))
+                {
+                    cmd.Parameters.AddWithValue("@Schema", tableName.Schema);
+                    cmd.Parameters.AddWithValue("@Name", tableName.Name);
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while(reader.Read())
+                        {
+                            var name = reader.GetString(reader.GetOrdinal("column_name"));
+                            var type = reader.GetString(reader.GetOrdinal("data_type"));
+
+                        }
+                    }
+                }
+            }
+            return columns;
         }
 
         protected override PostgreSqlObjectName ParseObjectName(string name)
