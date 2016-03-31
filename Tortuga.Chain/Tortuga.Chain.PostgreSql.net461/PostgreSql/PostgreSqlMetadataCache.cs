580a988da566362f674c68fa43d20b0898d9e6b7
docevaad <tomcik.david@gmail.com>
2016-03-30 22:20:45 -0700

    Added code to PostgresSqlMetadata

--- Tortuga.Chain/Tortuga.Chain.PostgreSql.net461/PostgreSql/PostgreSqlMetadataCache.cs
+++ Tortuga.Chain/Tortuga.Chain.PostgreSql.net461/PostgreSql/PostgreSqlMetadataCache.cs
@@ -1,50 +1,53 @@
 ﻿using Npgsql;
 using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Data;
 using Tortuga.Chain.Metadata;
 
 namespace Tortuga.Chain.PostgreSql
 {
-    public class PostgreSqlMetadataCache : DatabaseMetadataCache<string, DbType>
+    public class PostgreSqlMetadataCache : DatabaseMetadataCache<PostgreSqlObjectName, DbType>
     {
         private readonly NpgsqlConnectionStringBuilder m_ConnectionBuilder;
-        private readonly ConcurrentDictionary<string, TableOrViewMetadata<string, DbType>> m_Tables = new ConcurrentDictionary<string, TableOrViewMetadata<string, DbType>>();
+        private readonly ConcurrentDictionary<PostgreSqlObjectName, TableOrViewMetadata<PostgreSqlObjectName, DbType>> m_Tables = new ConcurrentDictionary<PostgreSqlObjectName, TableOrViewMetadata<PostgreSqlObjectName, DbType>>();
 
         public PostgreSqlMetadataCache(NpgsqlConnectionStringBuilder connectionBuilder)
         {
             m_ConnectionBuilder = connectionBuilder;
         }
 
-        public override StoredProcedureMetadata<string, DbType> GetStoredProcedure(string procedureName)
+        public override StoredProcedureMetadata<PostgreSqlObjectName, DbType> GetStoredProcedure(PostgreSqlObjectName procedureName)
         {
             throw new NotImplementedException();
         }
 
-        public override TableFunctionMetadata<string, DbType> GetTableFunction(string tableFunctionName)
+        public override TableFunctionMetadata<PostgreSqlObjectName, DbType> GetTableFunction(PostgreSqlObjectName tableFunctionName)
         {
             throw new NotImplementedException();
         }
 
-        public override TableOrViewMetadata<string, DbType> GetTableOrView(string tableName)
+        public override TableOrViewMetadata<PostgreSqlObjectName, DbType> GetTableOrView(PostgreSqlObjectName tableName)
         {
             return m_Tables.GetOrAdd(tableName, GetTableOrViewInternal);
         }
 
-        private TableOrViewMetadata<string, DbType> GetTableOrViewInternal(string tableName)
+        private TableOrViewMetadata<PostgreSqlObjectName, DbType> GetTableOrViewInternal(PostgreSqlObjectName tableName)
         {
             const string tableSql =
-                @"SELECT";
+                @"SELECT ";
+
+            return null;
         }
 
-        private List<ColumnMetadata<DbType>> GetColumns(string tableName)
+        private List<ColumnMetadata<DbType>> GetColumns(PostgreSqlObjectName tableName)
         {
-
+            return null;
         }
 
-        protected override string ParseObjectName(string name)
+        protected override PostgreSqlObjectName ParseObjectName(string name)
         {
-            return name;
+            return new PostgreSqlObjectName(name);
         }
     }
 }
