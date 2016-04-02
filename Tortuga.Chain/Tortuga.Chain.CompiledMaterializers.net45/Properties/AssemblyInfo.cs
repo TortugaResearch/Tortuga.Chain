25b8bcd1977f32dd1cc822ed70c02a8f806f8f13
Jonathan Allen <jonathanallen@kpmg.com>
2016-04-02 03:03:23 -0700

    Final package for the 0.9 release. Signed all published packages

---
+++ Tortuga.Chain/Tortuga.Chain.CompiledMaterializers.net45/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration(".NET Framework 4.5")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d1a446f3-354a-408d-bef6-860e6e5b4f81")]
---
+++ Tortuga.Chain/Tortuga.Chain.CompiledMaterializers.net46/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration(".NET Framework 4.6")]
+[assembly: ComVisible(false)]
+[assembly: Guid("49d257f5-365a-446c-83ca-aa29640f9ca5")]
