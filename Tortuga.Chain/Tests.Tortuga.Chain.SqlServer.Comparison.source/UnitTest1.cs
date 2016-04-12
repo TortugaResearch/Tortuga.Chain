2757a5ec954be4fc1fa0be4d9d7a5e3701aa6607
Jonathan Allen <jonathanallen@kpmg.com>
2016-04-12 15:39:43 -0700

    Work in progress

---
+++ Tortuga.Chain/Tests.Tortuga.Chain.SqlServer.Comparison.source/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tortuga.Chain;
+
+namespace Tests
+{
+    [TestClass]
+    public class DapperReadmeVsChain
+
+    {
+        private static SqlServerDataSource s_SqlServerDataSource;
+
+        static void DapperReadmeVsChain()
+        {
+            //This would be handled by your Dependency Injection framework
+            s_SqlServerDataSource = SqlServerDataSource.CreateFromConfig("CodeFirstModels");
+        }
+
+        public class Dog
+        {
+            public int? Age { get; set; }
+            public Guid Id { get; set; }
+            public string Name { get; set; }
+            public float? Weight { get; set; }
+
+            public int IgnoredProperty { get { return 1; } }
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
