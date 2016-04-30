d67d7bd2184eb558c2123337c822b1306cbad515
Jonathan Allen <jonathanallen@kpmg.com>
2016-04-30 11:58:50 -0700

    PostgreSQL assembly info

--- Tortuga.Chain/Tortuga.Chain.PostgreSql.source/Properties/AssemblyInfo.cs
+++ Tortuga.Chain/Tortuga.Chain.PostgreSql.source/Properties/AssemblyInfo.cs
@@ -1,9 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Tortuga.Chain.PostgreSql")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyConfiguration("Developer build. Not for production use.")]
 [assembly: ComVisible(false)]
 [assembly: Guid("98f3b971-7741-43f0-9be8-2a53f2a4f611")]
 
