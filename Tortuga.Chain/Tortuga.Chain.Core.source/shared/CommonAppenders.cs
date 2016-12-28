b10f080f9d4b26310c50c4d8dd974cfa50dda030
Jonathan Allen <jonathanallen@kpmg.com>
2016-12-28 02:27:38 -0800

    Code and tests are done for #38. Needs documentation.

--- Tortuga.Chain/Tortuga.Chain.Core.source/shared/CommonAppenders.cs
+++ Tortuga.Chain/Tortuga.Chain.Core.source/shared/CommonAppenders.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reflection;
+using Tortuga.Anchor.Metadata;
 using Tortuga.Chain.Appenders;
 using Tortuga.Chain.CommandBuilders;
-
-#if !WINDOWS_UWP
-#endif
 
 namespace Tortuga.Chain
 {
@@ -139,5 +139,146 @@ namespace Tortuga.Chain
         {
             return new StrictModeAppender<TResult>(previousLink, strictMode);
         }
+
+        /// <summary>
+        /// Joins a set of child objects to their parent objects.
+        /// </summary>
+        /// <typeparam name="T1">The type of the parent object.</typeparam>
+        /// <typeparam name="T2">The type of the child object.</typeparam>
+        /// <param name="previousLink">The previous link.</param>
+        /// <param name="joinExpression">The expression used to test of a parent and child match.</param>
+        /// <param name="targetCollectionExpression">The expression to get the collection on the parent to add the child to.</param>
+        /// <param name="joinOptions">The join options.</param>
+        public static ILink<List<T1>> Join<T1, T2>(this ILink<Tuple<List<T1>, List<T2>>> previousLink, Func<T1, T2, bool> joinExpression, Func<T1, ICollection<T2>> targetCollectionExpression, JoinOptions joinOptions = JoinOptions.None)
+        {
+            return new ExpressionJoinAppender<T1, T2>(previousLink, joinExpression, targetCollectionExpression, joinOptions);
+        }
+
+        /// <summary>
+        /// Joins a set of child objects to their parent objects.
+        /// </summary>
+        /// <typeparam name="T1">The type of the parent object.</typeparam>
+        /// <typeparam name="T2">The type of the child object.</typeparam>
+        /// <param name="previousLink">The previous link.</param>
+        /// <param name="joinExpression">The expression used to test of a parent and child match.</param>
+        /// <param name="targetCollectionName">The name of the collection property on the parent to add the child to.</param>
+        /// <param name="joinOptions">The join options.</param>
+        public static ILink<List<T1>> Join<T1, T2>(this ILink<Tuple<List<T1>, List<T2>>> previousLink, Func<T1, T2, bool> joinExpression, string targetCollectionName, JoinOptions joinOptions = JoinOptions.None)
+        {
+            return new ExpressionJoinAppender<T1, T2>(previousLink, joinExpression, targetCollectionName, joinOptions);
+        }
+
+
+        /// <summary>
+        /// Joins a set of child objects to their parent objects.
+        /// </summary>
+        /// <typeparam name="T1">The type of the parent object.</typeparam>
+        /// <typeparam name="T2">The type of the child object.</typeparam>
+        /// <typeparam name="TKey">The type of the primary/foreign key.</typeparam>
+        /// <param name="previousLink">The previous link.</param>
+        /// <param name="primaryKeyExpression">The expression to get the primary key from the parent object.</param>
+        /// <param name="foreignKeyExpression">The expression to get the foreign key from the child object.</param>
+        /// <param name="targetCollectionExpression">The expression to get the collection on the parent to add the child to.</param>
+        /// <param name="joinOptions">The join options.</param>
+        public static ILink<List<T1>> Join<T1, T2, TKey>(this ILink<Tuple<List<T1>, List<T2>>> previousLink, Func<T1, TKey> primaryKeyExpression, Func<T2, TKey> foreignKeyExpression, Func<T1, ICollection<T2>> targetCollectionExpression, JoinOptions joinOptions = JoinOptions.None)
+        {
+            return new KeyJoinAppender<T1, T2, TKey>(previousLink, primaryKeyExpression, foreignKeyExpression, targetCollectionExpression, joinOptions);
+        }
+
+        /// <summary>
+        /// Joins a set of child objects to their parent objects.
+        /// </summary>
+        /// <typeparam name="T1">The type of the parent object.</typeparam>
+        /// <typeparam name="T2">The type of the child object.</typeparam>
+        /// <typeparam name="TKey">The type of the primary/foreign key.</typeparam>
+        /// <param name="previousLink">The previous link.</param>
+        /// <param name="primaryKeyExpression">The expression to get the primary key from the parent object.</param>
+        /// <param name="foreignKeyExpression">The expression to get the foreign key from the child object.</param>
+        /// <param name="targetCollectionName">The name of the collection property on the parent to add the child to.</param>
+        /// <param name="joinOptions">The join options.</param>
+        public static ILink<List<T1>> Join<T1, T2, TKey>(this ILink<Tuple<List<T1>, List<T2>>> previousLink, Func<T1, TKey> primaryKeyExpression, Func<T2, TKey> foreignKeyExpression, string targetCollectionName, JoinOptions joinOptions = JoinOptions.None)
+        {
+            return new KeyJoinAppender<T1, T2, TKey>(previousLink, primaryKeyExpression, foreignKeyExpression, targetCollectionName, joinOptions);
+        }
+
+        /// <summary>
+        /// Joins a set of child objects to their parent objects.
+        /// </summary>
+        /// <typeparam name="T1">The type of the parent object.</typeparam>
+        /// <typeparam name="T2">The type of the child object.</typeparam>
+        /// <param name="previousLink">The previous link.</param>
+        /// <param name="primaryKeyName">The name of the property used to get the primary key from the parent object.</param>
+        /// <param name="foreignKeyName">The name of the property used to get the foreign key from the child object.</param>
+        /// <param name="targetCollectionName">The name of the collection property on the parent to add the child to.</param>
+        /// <param name="joinOptions">The join options.</param>
+        public static ILink<List<T1>> Join<T1, T2>(this ILink<Tuple<List<T1>, List<T2>>> previousLink, string primaryKeyName, string foreignKeyName, string targetCollectionName, JoinOptions joinOptions = JoinOptions.None)
+        {
+            var keyType = MetadataCache.GetMetadata(typeof(T1)).Properties[primaryKeyName].PropertyType;
+            var methodType = typeof(CommonAppenders).GetMethods(BindingFlags.NonPublic | BindingFlags.Static).Where(m => m.Name == "Join_Helper").Single();
+            var genericMethod = methodType.MakeGenericMethod(typeof(T1), typeof(T2), keyType);
+
+            return (ILink<List<T1>>)genericMethod.Invoke(null, new object[] { previousLink, primaryKeyName, foreignKeyName, targetCollectionName, joinOptions });
+        }
+
+        /// <summary>
+        /// Joins a set of child objects to their parent objects.
+        /// </summary>
+        /// <typeparam name="T1">The type of the parent object.</typeparam>
+        /// <typeparam name="T2">The type of the child object.</typeparam>
+        /// <param name="previousLink">The previous link.</param>
+        /// <param name="keyName">The name of the property used to get the primary key from the parent object and the foreign key from the child object.</param>
+        /// <param name="targetCollectionName">The name of the collection property on the parent to add the child to.</param>
+        /// <param name="joinOptions">The join options.</param>
+        public static ILink<List<T1>> Join<T1, T2>(this ILink<Tuple<List<T1>, List<T2>>> previousLink, string keyName, string targetCollectionName, JoinOptions joinOptions = JoinOptions.None)
+        {
+            var keyType = MetadataCache.GetMetadata(typeof(T1)).Properties[keyName].PropertyType;
+            var methodType = typeof(CommonAppenders).GetMethods(BindingFlags.NonPublic | BindingFlags.Static).Where(m => m.Name == "Join_Helper").Single();
+            var genericMethod = methodType.MakeGenericMethod(typeof(T1), typeof(T2), keyType);
+
+            return (ILink<List<T1>>)genericMethod.Invoke(null, new object[] { previousLink, keyName, keyName, targetCollectionName, joinOptions });
+        }
+
+        internal static ILink<List<T1>> Join_Helper<T1, T2, TKey>(ILink<Tuple<List<T1>, List<T2>>> previousLink, string primaryKeyName, string foreignKeyName, string targetCollectionName, JoinOptions joinOptions)
+        {
+            return new KeyJoinAppender<T1, T2, TKey>(previousLink, primaryKeyName, foreignKeyName, targetCollectionName, joinOptions);
+        }
+
+        /// <summary>
+        /// Joins a set of child objects to their parent objects.
+        /// </summary>
+        /// <typeparam name="T1">The type of the parent object.</typeparam>
+        /// <typeparam name="T2">The type of the child object.</typeparam>
+        /// <typeparam name="TKey">The type of the primary/foreign key.</typeparam>
+        /// <param name="previousLink">The previous link.</param>
+        /// <param name="primaryKeyName">The name of the property used to get the primary key from the parent object.</param>
+        /// <param name="foreignKeyName">The name of the property used to get the foreign key from the child object.</param>
+        /// <param name="targetCollectionName">The name of the collection property on the parent to add the child to.</param>
+        /// <param name="joinOptions">The join options.</param>
+        public static ILink<List<T1>> Join<T1, T2, TKey>(this ILink<Tuple<List<T1>, List<T2>>> previousLink, string primaryKeyName, string foreignKeyName, string targetCollectionName, JoinOptions joinOptions = JoinOptions.None)
+        {
+            return new KeyJoinAppender<T1, T2, TKey>(previousLink, primaryKeyName, foreignKeyName, targetCollectionName, joinOptions);
+        }
+
+        /// <summary>
+        /// Joins a set of child objects to their parent objects.
+        /// </summary>
+        /// <typeparam name="T1">The type of the parent object.</typeparam>
+        /// <typeparam name="T2">The type of the child object.</typeparam>
+        /// <typeparam name="TKey">The type of the primary/foreign key.</typeparam>
+        /// <param name="previousLink">The previous link.</param>
+        /// <param name="keyName">The name of the property used to get the primary key from the parent object and the foreign key from the child object.</param>
+        /// <param name="targetCollectionName">The name of the collection property on the parent to add the child to.</param>
+        /// <param name="joinOptions">The join options.</param>
+        public static ILink<List<T1>> Join<T1, T2, TKey>(this ILink<Tuple<List<T1>, List<T2>>> previousLink, string keyName, string targetCollectionName, JoinOptions joinOptions = JoinOptions.None)
+        {
+            if (string.IsNullOrEmpty(keyName))
+                throw new ArgumentException("keyName is null or empty.", "keyName");
+            //other parameters are checked by the constructor.
+
+            return new KeyJoinAppender<T1, T2, TKey>(previousLink, keyName, keyName, targetCollectionName, joinOptions);
+        }
     }
 }
+
+
+
