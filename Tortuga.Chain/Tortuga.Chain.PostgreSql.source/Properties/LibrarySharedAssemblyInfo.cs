e41b658904eb82d45338bef1aecacd9c3ad4aede
Jonathan Allen <grauenwolf@gmail.com>
2018-10-01 01:25:28 -0700

    Tortuga.Chain.PostgreSql.1.3.6848.2511.nupkg

--- Tortuga.Chain/Tortuga.Chain.PostgreSql.source/Properties/LibrarySharedAssemblyInfo.cs
+++ Tortuga.Chain/Tortuga.Chain.PostgreSql.source/Properties/LibrarySharedAssemblyInfo.cs
@@ -2,11 +2,9 @@
 
 [assembly: AssemblyTitle("Tortuga.Chain.PostgreSql")]
 [assembly: AssemblyDescription("Fluent ORM for .NET and PostgreSQL.")]
-
 [assembly: AssemblyCulture("")]
-
 #if NETSTANDARD2_0
-[assembly: AssemblyVersion("1.2")]
+[assembly: AssemblyVersion("1.3")]
 #else
-[assembly: AssemblyVersion("1.2.*")]
+[assembly: AssemblyVersion("1.3.*")]
 #endif
