dbb03e58e3f5514b6dbedb3fe4728fd4407591fb
Jonathan Allen <grauenwolf@gmail.com>
2018-10-01 01:08:41 -0700

    Tortuga.Chain.SqlServer.1.3.6848.1549.nupkg

--- Tortuga.Chain/Tortuga.Chain.CompiledMaterializers.source/Properties/LibrarySharedAssemblyInfo.cs
+++ Tortuga.Chain/Tortuga.Chain.CompiledMaterializers.source/Properties/LibrarySharedAssemblyInfo.cs
@@ -2,6 +2,5 @@
 
 [assembly: AssemblyTitle("Tortuga.Chain.CompiledMaterializers")]
 [assembly: AssemblyDescription("This library adds compiled versions of the ToObject and ToCollection materializers.")]
-
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyVersion("1.3.*")]
--- Tortuga.Chain/Tortuga.Chain.SqlServer.source/Properties/LibrarySharedAssemblyInfo.cs
+++ Tortuga.Chain/Tortuga.Chain.SqlServer.source/Properties/LibrarySharedAssemblyInfo.cs
@@ -2,12 +2,9 @@
 
 [assembly: AssemblyTitle("Tortuga.Chain.SqlServer")]
 [assembly: AssemblyDescription("Fluent ORM for .NET and SQL Server.")]
-
 [assembly: AssemblyCulture("")]
-
-
 #if NETSTANDARD1_3 || NETSTANDARD2_0
-[assembly: AssemblyVersion("1.2")]
+[assembly: AssemblyVersion("1.3")]
 #else
-[assembly: AssemblyVersion("1.2.*")]
+[assembly: AssemblyVersion("1.3.*")]
 #endif
