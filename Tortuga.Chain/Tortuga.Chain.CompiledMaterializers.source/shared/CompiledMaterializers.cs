bf56a5930c788e87a0643a2eff8b856d82f5813d
Jonathan Allen <grauenwolf@gmail.com>
2018-09-30 22:54:58 -0700

    Updated dependencies. Fixed bug in compiled materializers.

--- Tortuga.Chain/Tortuga.Chain.CompiledMaterializers.source/shared/CompiledMaterializers.cs
+++ Tortuga.Chain/Tortuga.Chain.CompiledMaterializers.source/shared/CompiledMaterializers.cs
@@ -18,7 +18,6 @@ namespace Tortuga.Chain
     /// </summary>
     public static class CompiledMaterializers
     {
-
         /// <summary>
         /// Allows compilation of the ToObject materializer.
         /// </summary>
@@ -32,7 +31,6 @@ namespace Tortuga.Chain
         {
             return new CompiledSingleRow<TCommand, TParameter>(commandBuilder);
         }
-
 
         /// <summary>
         /// Allows compilation of the ToObject and ToCollection materializer.
@@ -62,10 +60,8 @@ namespace Tortuga.Chain
             return new CompiledMultipleTable<TCommand, TParameter>(commandBuilder);
         }
 
-
-        class ColumnData
+        private class ColumnData
         {
-
             public ColumnData(int index, Type columnType, string getter)
             {
                 ColumnType = columnType;
@@ -73,11 +69,11 @@ namespace Tortuga.Chain
 
                 Index = index;
             }
+
             public int Index { get; }
             public string Getter { get; }
             public Type ColumnType { get; }
         }
-
 
         /// <summary>
         /// Creates the builder.
@@ -100,7 +96,6 @@ namespace Tortuga.Chain
             var typeName = MetadataCache.GetMetadata(typeof(TObject)).CSharpFullName;
 
             var changeTracker = typeof(TObject).GetInterfaces().Any(x => x == typeof(IChangeTracking));
-
 
             var columns = new Dictionary<string, ColumnData>(StringComparer.OrdinalIgnoreCase);
             for (var i = 0; i < reader.FieldCount; i++)
@@ -127,7 +122,6 @@ namespace Tortuga.Chain
                 columns.Add(columnName, new ColumnData(i, columnType, getter));
             }
 
-
             var evaluator = CSScript.RoslynEvaluator.Reset(false);
             //evaluator = evaluator.ReferenceAssemblyOf<string>();
             //evaluator = evaluator.ReferenceAssemblyOf<IDataReader>();
@@ -135,8 +129,15 @@ namespace Tortuga.Chain
             evaluator = evaluator.ReferenceAssemblyOf(reader);
             evaluator = AugmentScriptEvaluator(evaluator, typeof(TObject));
 
+            //We need a public version of the DataReader class.
+            //If we can't find one, default to the slower IDataReader interface.
+            var readerType = reader.GetType();
+            while (!readerType.IsPublic)
+                readerType = readerType.BaseType;
+            if (readerType.GetInterface("IDataReader") == null)
+                readerType = typeof(IDataReader);
 
-            code.AppendLine($"{typeName} Load({reader.GetType().FullName} reader)");
+            code.AppendLine($"{typeName} Load({readerType.FullName} reader)");
             code.AppendLine("{");
             code.AppendLine($"    var result = new {typeName}();");
 
@@ -156,7 +157,6 @@ namespace Tortuga.Chain
 
             code.AppendLine("    return result;");
             code.AppendLine("}");
-
 
             var codeToString = code.ToString();
             try
@@ -180,7 +180,6 @@ namespace Tortuga.Chain
                 //ex.Data["Evaluator"] = evaluator;
                 throw;
             }
-
         }
 
         /// <summary>
@@ -271,7 +270,6 @@ namespace Tortuga.Chain
                 }
                 else //type casting is required
                 {
-
                     var propertyTypeName = MetadataCache.GetMetadata(property.PropertyType).CSharpFullName;
 
                     if (property.PropertyType.IsClass || (property.PropertyType.Name == "Nullable`1" && property.PropertyType.IsGenericType))
@@ -300,9 +298,5 @@ namespace Tortuga.Chain
         /// Occurs when materializer fails to compile.
         /// </summary>
         public static event EventHandler<MaterializerCompilerEventArgs> MaterializerCompilerFailed;
-
     }
-
 }
-
-
