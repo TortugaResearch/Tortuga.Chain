854d3c8e5ba2962794dbdc8b6d902a6eef8b3003
Jonathan Allen <grauenwolf@gmail.com>
2018-12-24 00:03:19 -0800

    Tortuga.Chain.Core.2.0.6932.69

--- Tortuga.Chain/Tortuga.Chain.Core.source/Properties/LibrarySharedAssemblyInfo.cs
+++ Tortuga.Chain/Tortuga.Chain.Core.source/Properties/LibrarySharedAssemblyInfo.cs
@@ -3,7 +3,7 @@
 [assembly: AssemblyTitle("Tortuga.Chain.Core")]
 [assembly: AssemblyDescription("A fluent ORM for .NET.")]
 [assembly: AssemblyCulture("")]
-#if NETSTANDARD1_3 || NETSTANDARD2_0
+#if NETSTANDARD2_0
 [assembly: AssemblyVersion("2.0")]
 #else
 [assembly: AssemblyVersion("2.0.*")]
