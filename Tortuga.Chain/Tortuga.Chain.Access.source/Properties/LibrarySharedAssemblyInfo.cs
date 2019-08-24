80a67892c3372bbe57a9c56c75bd0d6e1e586ae7
Jonathan Allen <grauenwolf@gmail.com>
2019-08-23 20:41:01 -0700

    Convert to package references.

--- Tortuga.Chain/Tortuga.Chain.Access.source/Properties/LibrarySharedAssemblyInfo.cs
+++ Tortuga.Chain/Tortuga.Chain.Access.source/Properties/LibrarySharedAssemblyInfo.cs
@@ -4,7 +4,7 @@
 [assembly: AssemblyDescription("Fluent ORM for .NET and Access.")]
 [assembly: AssemblyCulture("")]
 #if NETSTANDARD1_3
-[assembly: AssemblyVersion("2.1")]
+[assembly: AssemblyVersion("2.2")]
 #else
-[assembly: AssemblyVersion("2.1.*")]
+[assembly: AssemblyVersion("2.2.*")]
 #endif
