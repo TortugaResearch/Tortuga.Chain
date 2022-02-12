498f0afc756b1b89d5b3de07730ba7ee7cfcc9ec
Jonathan Allen <grauenwolf@gmail.com>
2022-02-12 00:12:14 -0800

    TableAndView and GetByKey #408

--- Tortuga.Chain/Shared/Tests/Models/Employee.cs
+++ Tortuga.Chain/Shared/Tests/Models/Employee.cs
@@ -4,45 +4,63 @@ using Tortuga.Anchor.Modeling;
 
 namespace Tests.Models
 {
-    [Table("Employee", Schema = "HR")]
-    public class Employee
-    {
-        [IgnoreOnInsert, IgnoreOnUpdate]
-        public DateTime? CreatedDate { get; set; }
+	[Table("Employee", Schema = "HR")]
+	public class Employee
+	{
+		[IgnoreOnInsert, IgnoreOnUpdate]
+		public DateTime? CreatedDate { get; set; }
 
-        public string EmployeeId { get; set; } = Guid.NewGuid().ToString();
-        public int? EmployeeKey { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public int? ManagerKey { get; set; }
-        public string MiddleName { get; set; }
-        public string Title { get; set; }
+		public string EmployeeId { get; set; } = Guid.NewGuid().ToString();
+		public int? EmployeeKey { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public int? ManagerKey { get; set; }
+		public string MiddleName { get; set; }
+		public string Title { get; set; }
 
-        [IgnoreOnUpdate]
-        public DateTime? UpdatedDate { get; set; }
-    }
+		[IgnoreOnUpdate]
+		public DateTime? UpdatedDate { get; set; }
+	}
 
-    public class EmployeeWithName
-    {
-        public string EmployeeId { get; set; } = Guid.NewGuid().ToString();
-        public int? EmployeeKey { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-    }
+	public class EmployeeWithName
+	{
+		public string EmployeeId { get; set; } = Guid.NewGuid().ToString();
+		public int? EmployeeKey { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+	}
 
-    public class EmployeeWithoutKey
-    {
-        [IgnoreOnInsert, IgnoreOnUpdate]
-        public DateTime? CreatedDate { get; set; }
+	public class EmployeeWithoutKey
+	{
+		[IgnoreOnInsert, IgnoreOnUpdate]
+		public DateTime? CreatedDate { get; set; }
 
-        public string EmployeeId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public int? ManagerKey { get; set; }
-        public string MiddleName { get; set; }
-        public string Title { get; set; }
+		public string EmployeeId { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public int? ManagerKey { get; set; }
+		public string MiddleName { get; set; }
+		public string Title { get; set; }
 
-        [IgnoreOnUpdate]
-        public DateTime? UpdatedDate { get; set; }
-    }
+		[IgnoreOnUpdate]
+		public DateTime? UpdatedDate { get; set; }
+	}
+
+	[TableAndView("Employee", Schema = "HR", ViewName = "EmployeeWithManager")]
+	public class EmployeeWithView
+	{
+		[IgnoreOnInsert, IgnoreOnUpdate]
+		public DateTime? CreatedDate { get; set; }
+
+		public string EmployeeId { get; set; } = Guid.NewGuid().ToString();
+		public int? EmployeeKey { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public int? ManagerKey { get; set; }
+		public string MiddleName { get; set; }
+		public string Title { get; set; }
+
+		[IgnoreOnUpdate]
+		public DateTime? UpdatedDate { get; set; }
+	}
 }
