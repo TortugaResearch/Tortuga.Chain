e3303a85392fc2766b1fb74c36f011dfa9da332e
Jonathan Allen <grauenwolf@gmail.com>
2022-02-12 00:34:05 -0800

    Records and Strict Mode #407

--- Tortuga.Chain/Shared/Tests/Models/Employee.cs
+++ Tortuga.Chain/Shared/Tests/Models/Employee.cs
@@ -2,47 +2,71 @@ using System;
 using System.ComponentModel.DataAnnotations.Schema;
 using Tortuga.Anchor.Modeling;
 
+#if SQLITE
+using KeyType = System.Int64;
+#else
+using KeyType = System.Int32;
+#endif
+
 namespace Tests.Models
 {
-    [Table("Employee", Schema = "HR")]
-    public class Employee
-    {
-        [IgnoreOnInsert, IgnoreOnUpdate]
-        public DateTime? CreatedDate { get; set; }
+	[Table("Employee", Schema = "HR")]
+	public class Employee
+	{
+		[IgnoreOnInsert, IgnoreOnUpdate]
+		public DateTime? CreatedDate { get; set; }
 
-        public string EmployeeId { get; set; } = Guid.NewGuid().ToString();
-        public int? EmployeeKey { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public int? ManagerKey { get; set; }
-        public string MiddleName { get; set; }
-        public string Title { get; set; }
+		public string EmployeeId { get; set; } = Guid.NewGuid().ToString();
+		public int? EmployeeKey { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public int? ManagerKey { get; set; }
+		public string MiddleName { get; set; }
+		public string Title { get; set; }
 
-        [IgnoreOnUpdate]
-        public DateTime? UpdatedDate { get; set; }
-    }
+		[IgnoreOnUpdate]
+		public DateTime? UpdatedDate { get; set; }
+	}
 
-    public class EmployeeWithName
-    {
-        public string EmployeeId { get; set; } = Guid.NewGuid().ToString();
-        public int? EmployeeKey { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-    }
+	[Table("Employee", Schema = "HR")]
+	public record EmployeeRecord
+	{
+		[IgnoreOnInsert, IgnoreOnUpdate]
+		public DateTime? CreatedDate { get; set; }
 
-    public class EmployeeWithoutKey
-    {
-        [IgnoreOnInsert, IgnoreOnUpdate]
-        public DateTime? CreatedDate { get; set; }
+		public string EmployeeId { get; set; } = Guid.NewGuid().ToString();
+		public KeyType? EmployeeKey { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public KeyType? ManagerKey { get; set; }
+		public string MiddleName { get; set; }
+		public string Title { get; set; }
 
-        public string EmployeeId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public int? ManagerKey { get; set; }
-        public string MiddleName { get; set; }
-        public string Title { get; set; }
+		[IgnoreOnUpdate]
+		public DateTime? UpdatedDate { get; set; }
+	}
 
-        [IgnoreOnUpdate]
-        public DateTime? UpdatedDate { get; set; }
-    }
+	public class EmployeeWithName
+	{
+		public string EmployeeId { get; set; } = Guid.NewGuid().ToString();
+		public int? EmployeeKey { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+	}
+
+	public class EmployeeWithoutKey
+	{
+		[IgnoreOnInsert, IgnoreOnUpdate]
+		public DateTime? CreatedDate { get; set; }
+
+		public string EmployeeId { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public int? ManagerKey { get; set; }
+		public string MiddleName { get; set; }
+		public string Title { get; set; }
+
+		[IgnoreOnUpdate]
+		public DateTime? UpdatedDate { get; set; }
+	}
 }
