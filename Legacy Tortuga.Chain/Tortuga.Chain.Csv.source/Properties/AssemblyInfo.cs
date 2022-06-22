1cbc8b97a9b44fec02070f82ac1a5fff375aedcb
Jonathan Louis Allen <grauenwolf@gmail.com>
2022-06-22 10:22:40 -0700

    Remove legacy code. This will be stored in a separate branch for reference.

--- Legacy Tortuga.Chain/Tortuga.Chain.Csv.source/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyConfiguration("Development build")]
-[assembly: ComVisible(false)]
-[assembly: Guid("29250dd0-9a8e-49e9-bf19-5f45c54ac9fb")]
