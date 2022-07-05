5867b55d0aff7c2ca833fbd52c974e6b7ff53ba4
Jonathan Allen <grauenwolf@gmail.com>
2022-07-05 10:59:37 -0700

    ISupportsDeleteByKeyList should have the same overloads as ISupportsGetByKeyList #463

--- Tortuga.Chain/Shared/DataSource/CommandBuilders/SupportsDeleteByKeyListTrait`4.cs
+++ Tortuga.Chain/Shared/DataSource/CommandBuilders/SupportsDeleteByKeyListTrait`4.cs
@@ -21,23 +21,18 @@ class SupportsDeleteByKeyListTrait<TCommand, TParameter, TObjectName, TDbType> :
 	ISingleRowDbCommandBuilder ISupportsDeleteByKey.DeleteByKey(string tableName, string key, DeleteOptions options) => DeleteByKey(DataSource.DatabaseMetadata.ParseObjectName(tableName), key, options);
 
 	ISingleRowDbCommandBuilder<TObject> ISupportsDeleteByKey.DeleteByKey<TObject>(short key, DeleteOptions options)
-		where TObject : class
 		=> DeleteByKey<TObject>(key, options);
 
 	ISingleRowDbCommandBuilder<TObject> ISupportsDeleteByKey.DeleteByKey<TObject>(int key, DeleteOptions options)
-		where TObject : class
 		=> DeleteByKey<TObject>(key, options);
 
 	ISingleRowDbCommandBuilder<TObject> ISupportsDeleteByKey.DeleteByKey<TObject>(long key, DeleteOptions options)
-		where TObject : class
 		=> DeleteByKey<TObject>(key, options);
 
 	ISingleRowDbCommandBuilder<TObject> ISupportsDeleteByKey.DeleteByKey<TObject>(Guid key, DeleteOptions options)
-		where TObject : class
 		=> DeleteByKey<TObject>(key, options);
 
 	ISingleRowDbCommandBuilder<TObject> ISupportsDeleteByKey.DeleteByKey<TObject>(string key, DeleteOptions options)
-		where TObject : class
 		=> DeleteByKey<TObject>(key, options);
 
 	/// <summary>
@@ -132,6 +127,21 @@ class SupportsDeleteByKeyListTrait<TCommand, TParameter, TObjectName, TDbType> :
 		where TObject : class
 		=> new SingleRowDbCommandBuilder<TCommand, TParameter, TObject>(DataSource.OnDeleteByKeyList<string>(DataSource.DatabaseMetadata.GetTableOrViewFromClass<TObject>().Name, new List<string> { key }, options));
 
+	IMultipleRowDbCommandBuilder<TObject> ISupportsDeleteByKeyList.DeleteByKeyList<TObject, TKey>(IEnumerable<TKey> keys, DeleteOptions options)
+									=> DeleteByKeyList<TObject, TKey>(keys, options);
+
+	IMultipleRowDbCommandBuilder<TObject> ISupportsDeleteByKeyList.DeleteByKeyList<TObject>(IEnumerable<short> keys, DeleteOptions options)
+		=> DeleteByKeyList<TObject>(keys, options);
+
+	IMultipleRowDbCommandBuilder<TObject> ISupportsDeleteByKeyList.DeleteByKeyList<TObject>(IEnumerable<int> keys, DeleteOptions options)
+		=> DeleteByKeyList<TObject>(keys, options);
+
+	IMultipleRowDbCommandBuilder<TObject> ISupportsDeleteByKeyList.DeleteByKeyList<TObject>(IEnumerable<long> keys, DeleteOptions options)
+		=> DeleteByKeyList<TObject>(keys, options);
+
+	IMultipleRowDbCommandBuilder<TObject> ISupportsDeleteByKeyList.DeleteByKeyList<TObject>(IEnumerable<Guid> keys, DeleteOptions options)
+		=> DeleteByKeyList<TObject>(keys, options);
+
 	IMultipleRowDbCommandBuilder ISupportsDeleteByKeyList.DeleteByKeyList<TKey>(string tableName, IEnumerable<TKey> keys, DeleteOptions options) => DeleteByKeyList(DataSource.DatabaseMetadata.ParseObjectName(tableName), keys, options);
 
 	/// <summary>
@@ -145,4 +155,73 @@ class SupportsDeleteByKeyListTrait<TCommand, TParameter, TObjectName, TDbType> :
 	[Expose]
 	public MultipleRowDbCommandBuilder<TCommand, TParameter> DeleteByKeyList<TKey>(TObjectName tableName, IEnumerable<TKey> keys, DeleteOptions options = DeleteOptions.None)
 		=> DataSource.OnDeleteByKeyList(tableName, keys, options);
+
+	/// <summary>
+	/// Delete multiple rows by their primary key.
+	/// </summary>
+	/// <typeparam name="TObject">Used to determine the table name</typeparam>
+	/// <typeparam name="TKey"></typeparam>
+	/// <param name="keys">The keys.</param>
+	/// <param name="options">The options.</param>
+	/// <returns>MultipleRowDbCommandBuilder&lt;TCommand, TParameter&gt;.</returns>
+	[Expose]
+	public MultipleRowDbCommandBuilder<TCommand, TParameter, TObject> DeleteByKeyList<TObject, TKey>(IEnumerable<TKey> keys, DeleteOptions options = DeleteOptions.None)
+		where TObject : class
+		where TKey : struct
+		=> new MultipleRowDbCommandBuilder<TCommand, TParameter, TObject>(DataSource.OnDeleteByKeyList<TKey>(DataSource.DatabaseMetadata.GetTableOrViewFromClass<TObject>().Name, keys, options));
+
+	/// <summary>
+	/// Delete multiple rows by their primary key.
+	/// </summary>
+	/// <typeparam name="TObject">Used to determine the table name</typeparam>
+	/// <param name="keys">The keys.</param>
+	/// <param name="options">The options.</param>
+	[Expose]
+	public MultipleRowDbCommandBuilder<TCommand, TParameter, TObject> DeleteByKeyList<TObject>(IEnumerable<Guid> keys, DeleteOptions options = DeleteOptions.None)
+		where TObject : class
+		=> DeleteByKeyList<TObject, Guid>(keys, options);
+
+	/// <summary>
+	/// Delete multiple rows by their primary key.
+	/// </summary>
+	/// <typeparam name="TObject">Used to determine the table name</typeparam>
+	/// <param name="keys">The keys.</param>
+	/// <param name="options">The options.</param>
+	[Expose]
+	public MultipleRowDbCommandBuilder<TCommand, TParameter, TObject> DeleteByKeyList<TObject>(IEnumerable<long> keys, DeleteOptions options = DeleteOptions.None)
+		where TObject : class
+		=> DeleteByKeyList<TObject, long>(keys, options);
+
+	/// <summary>
+	/// Delete multiple rows by their primary key.
+	/// </summary>
+	/// <typeparam name="TObject">Used to determine the table name</typeparam>
+	/// <param name="keys">The keys.</param>
+	/// <param name="options">The options.</param>
+	[Expose]
+	public MultipleRowDbCommandBuilder<TCommand, TParameter, TObject> DeleteByKeyList<TObject>(IEnumerable<short> keys, DeleteOptions options = DeleteOptions.None)
+		where TObject : class
+		=> DeleteByKeyList<TObject, short>(keys, options);
+
+	/// <summary>
+	/// Delete multiple rows by their primary key.
+	/// </summary>
+	/// <typeparam name="TObject">Used to determine the table name</typeparam>
+	/// <param name="keys">The keys.</param>
+	/// <param name="options">The options.</param>
+	[Expose]
+	public MultipleRowDbCommandBuilder<TCommand, TParameter, TObject> DeleteByKeyList<TObject>(IEnumerable<int> keys, DeleteOptions options = DeleteOptions.None)
+	  where TObject : class
+		=> DeleteByKeyList<TObject, int>(keys, options);
+
+	/// <summary>
+	/// Delete multiple rows by their primary key.
+	/// </summary>
+	/// <typeparam name="TObject">Used to determine the table name</typeparam>
+	/// <param name="keys">The keys.</param>
+	/// <param name="options">The options.</param>
+	[Expose]
+	public MultipleRowDbCommandBuilder<TCommand, TParameter, TObject> DeleteByKeyList<TObject>(IEnumerable<string> keys, DeleteOptions options = DeleteOptions.None)
+		where TObject : class
+		=> new MultipleRowDbCommandBuilder<TCommand, TParameter, TObject>(DataSource.OnDeleteByKeyList<string>(DataSource.DatabaseMetadata.GetTableOrViewFromClass<TObject>().Name, keys, options));
 }
