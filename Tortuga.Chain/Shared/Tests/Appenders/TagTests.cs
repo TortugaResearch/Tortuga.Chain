24dbe52deea927362b3fdb5e9f930c39aecacda9
Jonathan Allen <grauenwolf@gmail.com>
2025-08-30 02:26:34 -0700

    Tag queries #401

---
+++ Tortuga.Chain/Shared/Tests/Appenders/TagTests.cs
@@ -0,0 +1,40 @@
+﻿using Tests.Models;
+using Tortuga.Chain;
+
+namespace Tests.Appenders;
+
+#if !ACCESS
+
+[TestClass]
+public class TagTests : TestBase
+{
+	[DataTestMethod, RootData(DataSourceGroup.All)]
+	public void TagTest(string dataSourceName)
+	{
+		var dataSource = DataSource(dataSourceName);
+		try
+		{
+			dataSource.From<Employee>().AsCount().Tag("This is my message").Execute();
+		}
+		finally
+		{
+			Release(dataSource);
+		}
+	}
+
+	[DataTestMethod, RootData(DataSourceGroup.All)]
+	public void TagSourceTest(string dataSourceName)
+	{
+		var dataSource = DataSource(dataSourceName);
+		try
+		{
+			dataSource.From<Employee>().AsCount().Tag().Execute();
+		}
+		finally
+		{
+			Release(dataSource);
+		}
+	}
+}
+
+#endif
---
+++ Tortuga.Chain/Tortuga.Chain.Core/Appenders/TagAppender`1.cs
@@ -0,0 +1,35 @@
+﻿using Tortuga.Chain.Core;
+
+namespace Tortuga.Chain.Appenders;
+
+
+/// <summary>
+/// Class TagAppender.
+/// </summary>
+internal sealed class TagAppender<TResult> : Appender<TResult>
+{
+	readonly string m_Message;
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="Appender{TResult}" /> class.
+	/// </summary>
+	/// <param name="previousLink">The previous link.</param>
+	/// <param name="message">The message to prepend to the SQL.</param>
+	public TagAppender(ILink<TResult> previousLink, string message)
+		: base(previousLink)
+	{
+		m_Message = message;
+	}
+
+	/// <summary>
+	/// Override this if you want to examine or modify the DBCommand before it is executed.
+	/// </summary>
+	/// <param name="e">The <see cref="CommandBuiltEventArgs" /> instance containing the event data.</param>
+	/// <exception cref="ArgumentNullException">nam - f(e), "e is</exception>
+	protected override void OnCommandBuilt(CommandBuiltEventArgs e)
+	{
+		if (e == null)
+			throw new ArgumentNullException(nameof(e), $"{nameof(e)} is null.");
+		e.Command.CommandText = "-- " + m_Message + "\r\n" + e.Command.CommandText;
+	}
+}
