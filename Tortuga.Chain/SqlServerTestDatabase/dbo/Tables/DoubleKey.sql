9408b0b804d20e30d22cb6821670ebe9afbfd274
Jonathan Allen <grauenwolf@gmail.com>
2025-09-15 19:10:55 -0700

    Create table for the tests.

---
+++ Tortuga.Chain/SqlServerTestDatabase/dbo/Tables/DoubleKey.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[DoubleKey]
+(
+	DoubleKeyId INT NOT NULL,
+	DoubleKeyCode VARCHAR(5) NOT NULL,
+	Value VARCHAR(20) NOT NULL,
+	CONSTRAINT PK_DoubleKey PRIMARY KEY (DoubleKeyId, DoubleKeyCode)
+)
